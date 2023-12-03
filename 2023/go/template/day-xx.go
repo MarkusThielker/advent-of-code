423f41bd6a354195ee93d40c770e0b69d4cbc4ae
Markus Thielker <mail.markus.thielker@gmail.com>
2023-12-03 13:01:04 +0100

    Add template files

---
+++ 2023/go/template/day-xx.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	utils "github.com/markusthielker/advent-of-code/2023/go"
+)
+
+func main() {
+
+	part1 := func(input []string) (result int) {
+		return len(input)
+	}
+
+	part2 := func(input []string) (result int) {
+		return len(input)
+	}
+
+	input := utils.ReadInput("day-xx/input")
+
+	fmt.Println(part1(input))
+	fmt.Println(part2(input))
+}
